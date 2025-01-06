98e06cf5d2d780bf7c7f22cbdef2f5c930031b43
wxk <418306613@qq.com>
2025-01-06 22:04:25 +0800

    添加官方web demo

---
+++ run_test.sh
@@ -0,0 +1,10 @@
+
+
+
+###对单个视频进行动作捕捉
+python tools/demo/demo.py --video=./dance1.mp4 -s
+
+
+
+###对一个文件夹下的多个视频，进行动作捕捉
+python tools/demo/demo_folder.py -f inputs/demo/folder_in -d outputs/demo/folder_out -s
